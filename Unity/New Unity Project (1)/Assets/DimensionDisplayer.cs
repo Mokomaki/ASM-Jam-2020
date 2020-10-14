5ceaf161b8f06de72234bfc5284fee9b65e57fcf
Mokomaki <grest.hyber@gmail.com>
2020-10-15 00:51:08 +0300

    made game

---
+++ Unity/New Unity Project (1)/Assets/DimensionDisplayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DimensionDisplayer : MonoBehaviour
+{
+    [SerializeField] Image[] m_IMGs;
+    [SerializeField] Sprite m_sprSelected;
+    [SerializeField] Sprite m_sprUnSelected;
+
+    int m_lastDIM = 1;
+    // Update is called once per frame
+    private void Start()
+    {
+        Show(1);
+    }
+
+    void FixedUpdate()
+    {
+        if(m_lastDIM!=MultiDimesionalObject.s_DimensionShift)
+        {
+            Show(MultiDimesionalObject.s_DimensionShift);
+            m_lastDIM = MultiDimesionalObject.s_DimensionShift;
+        }
+    }
+
+    void Show(int dim)
+    {
+
+        for (int i = 0; i < m_IMGs.Length; i++)
+        {
+            m_IMGs[i].sprite = (dim-1==i) ? m_sprUnSelected : m_sprSelected;
+        }
+    }
+
+}
--- Unity/New Unity Project (1)/Assets/Scripts/PlayerControl.cs
+++ Unity/New Unity Project (1)/Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,123 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 respawnPoint;
+
+    public float rayDist;
+
+    private void Start()
     {
-        
+        respawnPoint = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        MultiDimesionalObject.s_DimensionShift += Mathf.RoundToInt(Input.mouseScrollDelta.y);
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Shift(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Shift(2);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            Shift(3);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            Shift(4);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            Shift(5);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            Shift(6);
+        }
+        
+        
+        Debug.DrawRay(transform.position, new Vector3(0, -1, 1), Color.red);
+        Debug.DrawRay(transform.position, new Vector3(0, -1, -1), Color.red);
+        Debug.DrawRay(transform.position, new Vector3(-1, -1, 0), Color.red);
+        Debug.DrawRay(transform.position, new Vector3(1, -1, 0), Color.red);
+
+        Movement();
+    }
+
+    void Shift(int dimension)
+    {
+        for(int i = 0; i<MultiDimesionalObject.s_MDOs.Count;i++)
+        {
+            if(MultiDimesionalObject.s_MDOs[i].m_shifting)
+            {
+                return;
+            }
+        }
+        MultiDimesionalObject.s_DimensionShift = dimension;
+    }
+
+    void Movement()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (Physics.Raycast(new Ray(transform.position, new Vector3(0, -1, 1)), rayDist))
+            {
+                transform.Translate(new Vector3(0, 0, 1));
+                
+                //TODO ei näin
+                transform.GetChild(0).rotation = Quaternion.identity;
+                transform.GetChild(0).Rotate(0,-90,0);
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            
+            if (Physics.Raycast(new Ray(transform.position, new Vector3(0, -1, -1)), rayDist))
+            {
+                transform.Translate(new Vector3(0, 0, -1));
+                //TODO ei näin
+                transform.GetChild(0).rotation = Quaternion.identity;
+                transform.GetChild(0).Rotate(0, 90, 0);
+
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.A))
+        {
+            
+            if (Physics.Raycast(new Ray(transform.position, new Vector3(-1, -1, 0)), rayDist))
+            {
+                transform.Translate(new Vector3(-1, 0, 0));
+                //TODO ei näin
+                transform.GetChild(0).rotation = Quaternion.identity;
+                transform.GetChild(0).Rotate(0, 180, 0);
+
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            
+            if (Physics.Raycast(new Ray(transform.position, new Vector3(1, -1, 0)), rayDist))
+            {
+                transform.Translate(new Vector3(1, 0, 0));
+                //TODO ei näin
+                transform.GetChild(0).rotation = Quaternion.identity;
+
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("RespawnPoint"))
+        {
+            respawnPoint = other.transform.position;
+        }
+        if(other.CompareTag("kill"))
+        {
+            //GetComponent<Rigidbody>().velocity = Vector3.zero;
+            transform.position = respawnPoint+new Vector3(0,2,0);
+        }
     }
 }
