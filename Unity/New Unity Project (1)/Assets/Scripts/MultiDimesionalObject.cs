3654dcb5c8f84dada423415c654b8bbb46f6d22f
Mokomaki <grest.hyber@gmail.com>
2020-10-14 17:09:27 +0300

    Initial commit.

---
+++ Unity/New Unity Project (1)/Assets/Scripts/MultiDimesionalObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiDimesionalObject : MonoBehaviour
+{
+    public static int s_DimensionShift = 0;
+    [SerializeField] int m_DimensionShiftIndex = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_DimensionShiftIndex!=s_DimensionShift)
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+}
